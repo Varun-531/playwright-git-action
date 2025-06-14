972c3cd9e2b2fa9e5fe6f00c057eeb123b501504
ChVarunFE <chennaparapu.varun@razorpay.com>
2025-06-14 16:04:19 +0530

    failing test

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,6 +1,6 @@
 import { test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:5173");
+  await page.goto("http://localhost:51731111");
   await page.getByRole("heading", { name: "useCallback Example" }).click();
 });
