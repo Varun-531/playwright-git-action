b6759183b9bf91b8639e5639332301fa2138dc38
Varun-531 <126848991+Varun-531@users.noreply.github.com>
2025-06-14 17:08:12 +0530

    failing test (#3)
    * failing test
    
    * Modify test
    
    * reuse existing server
    
    * failing test
    
    * minor test
    
    ---------
    
    Co-authored-by: ChVarunFE <chennaparapu.varun@razorpay.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-// App.js
-import React, { useState, useCallback } from "react";
+import React from "react";
 
-// Button component
-const IncrementButton = React.memo(({ onIncrement }) => {
-  console.log("IncrementButton re-rendered");
-  return <button onClick={onIncrement}>Increment</button>;
-});
-
-function App() {
-  const [count, setCount] = useState(0);
-  const [otherState, setOtherState] = useState(false);
-
-  // useCallback will return the same function unless `count` changes
-  const increment = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, []);
-
+const App = () => {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>useCallback Example</h1>
-      <p>Count: {count}</p>
-      <IncrementButton onIncrement={increment} />
-
-      <button onClick={() => setOtherState(!otherState)}>
-        Toggle Other State
-      </button>
-      <p>Other State: {otherState.toString()}</p>
+    <div>
+      <h1>Hello World</h1>
+      <a href="https://www.google.com">Testing link</a>
     </div>
   );
-}
+};
 
 export default App;
--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,6 +1,12 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:5173");
-  await page.getByRole("heading", { name: "useCallback Example" }).click();
+  await page.goto("/");
+  await page.getByRole("heading", { name: "Hello World" }).click();
+});
+
+test("has link", async ({ page }) => {
+  await page.goto("/");
+  await page.getByRole("link", { name: "Testing link" }).click();
+  await expect(page).toHaveURL("https://www.google.com");
 });
